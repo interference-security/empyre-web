81c58a7c530900e8dd029416290f7fee4ea58044
Sean Pierce <seaniepierce@gmail.com>
2017-03-22 17:01:43 -0400

    Update about.php

--- about.php
+++ about.php
@@ -13,7 +13,7 @@
             <div class="panel panel-primary">
                 <div class="panel-heading"><span class='glyphicon glyphicon-info-sign'></span> About EmPyre Web v1.0</div>
                 <div class="panel-body">
-                    EmPyre Web utilizes the <a href="https://github.com/adaptivethreat/EmPyre">EmPyre REST API Server</a>. It is a web interface for using <a href="https://github.com/adaptivethreat/EmPyre">EmPyre</a>.<br><br>
+                    EmPyre Web utilizes the <a href="https://github.com/interference-security/empyre-web/">EmPyre REST API Server</a>. It is a web interface for using <a href="https://github.com/EmpireProject/EmPyre">EmPyre</a>.<br><br>
                     EmPyre Web created by <a href="https://twitter.com/xploresec">@xploresec</a><br><br>
                     <a href="https://twitter.com/xploresec"><img src="img/twitter.png" style="width:48px; height:48px;"></a> &nbsp;&nbsp;&nbsp; 
                     <a href="https://github.com/interference-security/empyre-web/"><img src="img/github.png" style="width:48px; height:48px;"></a>
